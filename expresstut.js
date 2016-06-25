fbfb8bc09ccc7ccbfc923bb42b566896716ba2cf
Steven Jay <stevenjasionowicz@gmail.com>
2016-06-25 13:30:46 +1200

    Add routes, 404 and 500

--- expresstut.js
+++ expresstut.js
@@ -19,6 +19,37 @@ app.get('/', function(req, res){
 	res.render('home');
 });
 
+app.get('/about', function(req, res){
+	res.render('about');
+});
+
+app.use(function(req, res){
+	res.type('text/html');
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
+});
+
+app.use(function(req, res, next){
+	console.log("Looking for URL : " + req.url);
+	next();
+});
+
+app.get('/junk', function(req, res, next){
+	console.log('Tried to access /junk');
+	throw new Error('/junk doesn\'t exist');
+});
+
+app.use(function(err, req, res, next){
+	console.log('Error : ' + err.message);
+	next();
+});
+
 app.listen(app.get('port'), function(){
 	console.log("Express started on http://localhost:" + app.get('port') + " press Ctrl-C to terminate");
 });
