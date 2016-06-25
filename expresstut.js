0414a8991fd6f7d330900fe57903773b0fd70615
Steven Jay <stevenjasionowicz@gmail.com>
2016-06-25 13:17:33 +1200

    Add layout and build server

--- expresstut.js
+++ expresstut.js
@@ -2,13 +2,23 @@ var express = require('express');
 
 var app = express();
 
+app.disable('x-powered-by');
+
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+
+//More Imports Here
+
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', function(req, res) {
-	res.send('Express Works');
+app.use(express.static(__dirname + '/public'));
 
+app.get('/', function(req, res){
+	res.render('home');
 });
 
-app.listen(app.get('port'), function() {
-	console.log('Express started press Ctrl-C to terminate'); 
-})
+app.listen(app.get('port'), function(){
+	console.log("Express started on http://localhost:" + app.get('port') + " press Ctrl-C to terminate");
+});
